f1030dc39fc5e476b262e58ebcbd525b6de30a20
Malvarez88 <marianoalvarez66@gmail.com>
2022-09-06 17:48:19 -0300

    sigin backend

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,15 +1,29 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-
-import User from '../models/user.js'
+import User from "../models/user.js";
 
 export const signIn = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser)
+      return res.status(404).json({ message: "User doesnt exist." });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid credentials." });
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ message: existingUser, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong." });
+  }
+};
 
-
-}
-
-export const signUp = async (req, res) => {
-    
-
-}
+export const signUp = async (req, res) => {};
